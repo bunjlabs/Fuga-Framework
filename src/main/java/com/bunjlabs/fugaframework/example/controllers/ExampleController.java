69c17b1a622ad6f6e1a5dd53be72ab46786bdf4a
Artem Shurygin <show.vars@gmail.com>
2015-11-21 20:31:41 +1000

    Try to find a bug

--- src/main/java/com/bunjlabs/fugaframework/example/controllers/ExampleController.java
+++ src/main/java/com/bunjlabs/fugaframework/example/controllers/ExampleController.java
@@ -1,27 +1,29 @@
 package com.bunjlabs.fugaframework.example.controllers;
 
-
 import com.bunjlabs.fugaframework.foundation.Context;
 import com.bunjlabs.fugaframework.foundation.Controller;
 import com.bunjlabs.fugaframework.foundation.Response;
+import com.bunjlabs.fugaframework.templates.TemplateNotFoundException;
+import com.bunjlabs.fugaframework.templates.TemplateRenderException;
 import java.nio.charset.Charset;
 
 public class ExampleController extends Controller {
-    
+
     public static Response skip(Context ctx) {
         ctx.getSession().put("Key", "Some important string");
-        
+
         ctx.put("key", "Context value");
         return proceed();
     }
-    
-    public static Response index(Context ctx, String one, int to) {
-        return ok("Hello! " + one + " - " + to + " = " + ctx.getSession().getString("Key") +
-                " | " + ctx.get("key", String.class));
+
+    public static Response index(Context ctx, String one, int to) throws TemplateNotFoundException, TemplateRenderException {
+        ctx.put("data", "Hello! " + one + " - " + to + " = " + ctx.getSession().getString("Key")
+                + " | " + ctx.get("key", String.class));
+        return ok(view(ctx, "example/index.html"));
     }
-    
+
     public static Response post(Context ctx) {
-        
+
         return ok(ctx.getRequest().getContent().toString(Charset.forName("UTF-8")));
     }
 }
